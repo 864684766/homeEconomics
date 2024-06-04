5df3b8db43e9a85cd44b45ffc15e391529c768e6
e-lianwei.he <864684766@qq.com>
2024-06-04 10:39:02 +0800

    项目添加

---
+++ pages/index/components/btnGroup.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="btn-group">
+		<uni-grid :column="4" :highlight="true" @change="gridChangeHandle" :square="false" :showBorder="false">
+			<uni-grid-item v-for="(item, index) in btns" :index="index" :key="index">
+				<view class="grid-item-box">
+					<image mode="scaleToFill" style="width: 60rpx;height: 60rpx" :src="item.icon" @error="imageError" />
+					<text class="grid-item-text">{{item.text}}</text>
+				</view>
+			</uni-grid-item>
+		</uni-grid>
+	</view>
+</template>
+
+<script setup>
+	import {
+		ref
+	} from 'vue';
+
+	const btns = ref([{
+		icon: '/static/imgs/index/lottery.svg',
+		text: '领券中心'
+	}, {
+		icon: '/static/imgs/index/active.svg',
+		text: '活动中心'
+	}, {
+		icon: '/static/imgs/index/integral.svg',
+		text: '积分兑换'
+	}, {
+		icon: '/static/imgs/index/business.svg',
+		text: '业务分类'
+	}])
+
+	const gridChangeHandle = () => {
+
+	}
+
+	const imageError = (err) => {
+		console.log(err);
+	}
+</script>
+
+<style lang="scss">
+	.btn-group {
+		margin-top: 30rpx;
+	}
+
+	.grid-item-box {
+		background-color: #fff;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		.grid-item-text {
+			margin-top: 5rpx;
+			font-size: 16rpx;
+		}
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="content">
+		<view class="uni-margin-wrap">
+			<swiper class="swiper" circular indicator-dots autoplay>
+				<swiper-item v-for="(item,index) in banner_list">
+					<image mode="scaleToFill" style="width: 100%;height: 100%;" :src="item" @error="imageError" />
+				</swiper-item>
+			</swiper>
+			<btn-group />
+		</view>
+	</view>
+</template>
+
+<script lang="ts" setup>
+	import { ref } from 'vue';
+	import btnGroup from './components/btnGroup.vue';
+	const banner_list = ref(['/static/imgs/index/1.jpeg', '/static/imgs/index/2.png', '/static/imgs/index/3.webp'])
+
+	const imageError = (err) => {
+		console.log(err);
+	}
+</script>
+
+<style lang="scss">
+
+</style>
