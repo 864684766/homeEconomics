ce22d41d4c7ac9de9cd3a313a7ef992ebfd0b5d8
e-lianwei.he <864684766@qq.com>
2024-06-07 17:44:32 +0800

    fix: BUG修改

--- pages/integral/index.vue
+++ pages/integral/index.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script lang="ts" setup>
-	import { onBeforeMount } from 'vue';
+	import { onBeforeMount, onMounted } from 'vue';
 	onBeforeMount(() => {
 		// #ifndef H5
 		uni.showShareMenu({
@@ -13,6 +13,21 @@
 		})
 		// #endif
 	})
+
+	// 分享给朋友
+	const onShareAppMessage = (res) => {
+		console.log('onShareAppMessage---', res);
+		return {
+			title: '冲100送300',
+			path: '/pages/index/index',
+		};
+	};
+
+	onMounted(() => {
+		const pages = getCurrentPages();
+		const thisPage = pages[pages.length - 1];
+		thisPage.onShareAppMessage = onShareAppMessage;
+	});
 </script>
 
 <style>
